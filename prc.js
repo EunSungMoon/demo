48d806a776500dd2e4baa12f2bb10ea2f134f5d5
EunSungMoon <eunsung9647@gmail.com>
2021-04-21 14:27:57 +0900

    기분 버튼 생성& 출력

--- prc.js
+++ prc.js
@@ -1,1 +1,10 @@
-//소스작성 하면됨
+function mood(str) {
+    if (str == "joy")
+    console.log("joy");
+
+    else if (str == "sadness")
+    console.log("sadness");
+
+    else if (str == "anger")
+    console.log("anger");
+    }
