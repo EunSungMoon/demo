db7284b81238761f1283c2aafbb41f6954fa20c5
EunSungMoon <eunsung9647@gmail.com>
2021-04-28 19:50:53 +0900

    reset버튼 생성, 기능

--- prc.js
+++ prc.js
@@ -1,24 +1,28 @@
 function mood(str) {
   let MovieImages = [
     {
+      id: "Joy",
       src: "./MovieImages/joy.jpg",
-
     },
     {
+      id: "Anger",
       src: "./MovieImages/anger.jpg",
-
     },
     {
+      id: "Sadness",
       src: "./MovieImages/sadness.jpg",
-
     }
   ]
+
+
 
   let ImageArray = []
   for (i = 0, j = MovieImages.length; i < j; i++) {
     ImageArray[i] = new Image(240, 300)
     ImageArray[i].src = MovieImages[i].src
   }
+  
+
 
   if (str == "Joy")
     document.body.appendChild(ImageArray[0])
@@ -28,6 +32,12 @@ function mood(str) {
 
   else if (str == "Anger")
     document.body.appendChild(ImageArray[2])
-
 }
+
+function Reset(){
+  console.log('reset');
+  let reset=document.getElementsByTagName('img')
+  ImageArray.parentNode.removeChild(ImageArray)
+}
+
 
